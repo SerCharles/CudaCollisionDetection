fe713fabb7b42092383fa62f3105f89109405e73
Charles Shen <ssa19990208@foxmail.com>
2020-12-29 21:30:49 +0800

    cuda_brute_force

--- src/CudaCollisionDetection/Collision.cu
+++ src/CudaCollisionDetection/Collision.cu
@@ -1,6 +1,20 @@
 #include"Collision.cuh"
 #include "Ball.hpp"
 
+__device__ float Dist(float x, float y, float z)
+{
+	return sqrt(x * x + y * y + z * z);
+}
+
+__device__ float Dist(Point& p)
+{
+	return sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+}
+
+__device__ float Multiply(Point& a, Point& b)
+{
+	return (a.x * b.x + a.y * b.y + a.z * b.z);
+}
 
 /*
 	描述：处理与边界相撞
@@ -50,13 +64,14 @@ __device__ void HandleCollisionBoard(Ball& ball, float XRange, float ZRange, flo
 */
 __device__ void BallMove(Ball& ball, float time, float XRange, float ZRange, float Height)
 {
+
 	ball.CurrentPlace.x = ball.CurrentPlace.x + ball.CurrentSpeed.x * time;
 	ball.CurrentPlace.y = ball.CurrentPlace.y + ball.CurrentSpeed.y * time;
 	ball.CurrentPlace.z = ball.CurrentPlace.z + ball.CurrentSpeed.z * time;
 	HandleCollisionBoard(ball, XRange, ZRange, Height);
 }
 
-__global__ void UpdateBallsMoveNaive(Ball* balls, float TimeOnce, float XRange, float ZRange, float Height, int N)
+__global__ void UpdateBallsMove(Ball* balls, float TimeOnce, float XRange, float ZRange, float Height, int N)
 {
 	// 获取全局索引
 	int index = threadIdx.x + blockIdx.x * blockDim.x;
@@ -67,6 +82,100 @@ __global__ void UpdateBallsMoveNaive(Ball* balls, float TimeOnce, float XRange, 
 		BallMove(balls[i], TimeOnce, XRange, ZRange, Height);
 	}
 
+}
+
+/*
+	描述：判断两个球是否相撞
+	参数：球a，球b
+	返回：是1，否0
+*/
+__device__ bool JudgeCollision(Ball& a, Ball& b)
+{
+	float dist = 0;
+	float dist_x = a.CurrentPlace.x - b.CurrentPlace.x;
+	float dist_y = a.CurrentPlace.y - b.CurrentPlace.y;
+	float dist_z = a.CurrentPlace.z - b.CurrentPlace.z;
+	dist = Dist(dist_x, dist_y, dist_z);
+	if (dist < a.Radius + b.Radius)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+/*
+	描述：两球相撞后更新速度
+	参数：球a，球b
+	返回：无
+*/
+__device__ void ChangeSpeed(Ball& a, Ball& b)
+{
+	//径向速度按照质量做变换，法向速度不变
+	float dist = 0;
+	float diff_x = b.CurrentPlace.x - a.CurrentPlace.x;
+	float diff_y = b.CurrentPlace.y - a.CurrentPlace.y;
+	float diff_z = b.CurrentPlace.z - a.CurrentPlace.z;
+	dist = Dist(diff_x, diff_y, diff_z);
+
+	//求径向，法向速度
+	float rate_collide_a = (a.CurrentSpeed.x * diff_x + a.CurrentSpeed.y * diff_y + a.CurrentSpeed.z * diff_z) / dist / dist;
+	float speed_collide_a_x = diff_x * rate_collide_a;
+	float speed_collide_a_y = diff_y * rate_collide_a;
+	float speed_collide_a_z = diff_z * rate_collide_a;
+
+	float rate_collide_b = (b.CurrentSpeed.x * diff_x + b.CurrentSpeed.y * diff_y + b.CurrentSpeed.z * diff_z) / dist / dist;
+	float speed_collide_b_x = diff_x * rate_collide_b;
+	float speed_collide_b_y = diff_y * rate_collide_b;
+	float speed_collide_b_z = diff_z * rate_collide_b;
+
+	float unchanged_a_x = a.CurrentSpeed.x - speed_collide_a_x;
+	float unchanged_a_y = a.CurrentSpeed.y - speed_collide_a_y;
+	float unchanged_a_z = a.CurrentSpeed.z - speed_collide_a_z;
+
+	float unchanged_b_x = b.CurrentSpeed.x - speed_collide_b_x;
+	float unchanged_b_y = b.CurrentSpeed.y - speed_collide_b_y;
+	float unchanged_b_z = b.CurrentSpeed.z - speed_collide_b_z;
+
+
+	//假设b不动，a撞b，更新两者径向速度
+	float speed_collide_new_a_x = (speed_collide_a_x * (a.Weight - b.Weight) + speed_collide_b_x * (2 * b.Weight)) / (a.Weight + b.Weight);
+	float speed_collide_new_a_y = (speed_collide_a_y * (a.Weight - b.Weight) + speed_collide_b_y * (2 * b.Weight)) / (a.Weight + b.Weight);
+	float speed_collide_new_a_z = (speed_collide_a_z * (a.Weight - b.Weight) + speed_collide_b_z * (2 * b.Weight)) / (a.Weight + b.Weight);
+
+	float speed_collide_new_b_x = (speed_collide_a_x * (2 * a.Weight) + speed_collide_b_x * (b.Weight - a.Weight)) / (a.Weight + b.Weight);
+	float speed_collide_new_b_y = (speed_collide_a_y * (2 * a.Weight) + speed_collide_b_y * (b.Weight - a.Weight)) / (a.Weight + b.Weight);
+	float speed_collide_new_b_z = (speed_collide_a_z * (2 * a.Weight) + speed_collide_b_z * (b.Weight - a.Weight)) / (a.Weight + b.Weight);
+
+	a.CurrentSpeed.x = speed_collide_new_a_x + unchanged_a_x;
+	a.CurrentSpeed.y = speed_collide_new_a_y + unchanged_a_y;
+	a.CurrentSpeed.z = speed_collide_new_a_z + unchanged_a_z;
+
+	b.CurrentSpeed.x = speed_collide_new_b_x + unchanged_b_x;
+	b.CurrentSpeed.y = speed_collide_new_b_y + unchanged_b_y;
+	b.CurrentSpeed.z = speed_collide_new_b_z + unchanged_b_z;
+}
+
+__global__ void HandleCollisionNaive(Ball* balls, int N)
+{
+	// 获取全局索引
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	// 步长
+	int stride = blockDim.x * gridDim.x;
+	for (int k = index; k < N * N; k += stride)
+	{
+		int i = k / N;
+		int j = k % N;
+		if(i < j)
+		{
+			if (JudgeCollision(balls[i], balls[j]))
+			{
+				ChangeSpeed(balls[i], balls[j]);
+			}
+		}
+	}
 }
 
 void UpdateBallsNaiveGPU(Ball* balls, float TimeOnce, float XRange, float ZRange, float Height, int N)
@@ -82,15 +191,20 @@ void UpdateBallsNaiveGPU(Ball* balls, float TimeOnce, float XRange, float ZRange
 	// 定义kernel的执行配置
 	dim3 blockSize(256);
 	dim3 gridSize((N + blockSize.x - 1) / blockSize.x);
-	// 执行kernel
-	UpdateBallsMoveNaive <<< gridSize, blockSize >>> (balls_gpu, TimeOnce, XRange, ZRange, Height, N);
 
+	// 执行kernel
+	HandleCollisionNaive << < gridSize, blockSize >> > (balls_gpu, N);
+	// 同步device 保证结果能正确访问
+	cudaDeviceSynchronize();
+
+	// 执行kernel
+	UpdateBallsMove <<< gridSize, blockSize >>> (balls_gpu, TimeOnce, XRange, ZRange, Height, N);
 	// 同步device 保证结果能正确访问
 	cudaDeviceSynchronize();
 
 	// 记录结果
-	cudaMemcpy((void*)balls, (void*)balls_gpu, nBytes, cudaMemcpyHostToDevice);
+	cudaMemcpy((void*)balls, (void*)balls_gpu, nBytes, cudaMemcpyDeviceToHost);
 
 	// 释放内存
-	cudaFree(balls);
+	cudaFree(balls_gpu);
 }
--- src/CudaCollisionDetection/main.cpp
+++ src/CudaCollisionDetection/main.cpp
@@ -43,6 +43,17 @@ void InitWindow()
 	const GLubyte* gluVersion = gluGetString(GLU_VERSION);
 	printf("OpenGL实现的版本号：%s\n", OpenGLVersion);
 	printf("OGLU工具库版本：%s\n", gluVersion);
+	int dev = 0;
+	cudaDeviceProp devProp;
+	if (cudaGetDeviceProperties(&devProp, dev) == cudaSuccess)
+	{
+		std::cout << "使用GPU device " << dev << ": " << devProp.name << std::endl;
+		std::cout << "SM的数量：" << devProp.multiProcessorCount << std::endl;
+		std::cout << "每个线程块的共享内存大小：" << devProp.sharedMemPerBlock / 1024.0 << " KB" << std::endl;
+		std::cout << "每个线程块的最大线程数：" << devProp.maxThreadsPerBlock << std::endl;
+		std::cout << "每个EM的最大线程数：" << devProp.maxThreadsPerMultiProcessor << std::endl;
+		std::cout << "每个EM的最大线程束数：" << devProp.maxThreadsPerMultiProcessor / 32 << std::endl;
+	}
 }
 
 //初始化光照
